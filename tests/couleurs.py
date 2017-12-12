bc1d5b4f406f2f8c3b625d2d968506a97d6a90be
theod <theo.depresle@gmail.com>
2017-12-12 12:40:47 +0100

    Ajout des couleurs avec le calcul HSV

--- tests/couleurs.py
+++ tests/couleurs.py
@@ -2,38 +2,41 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from PIL import Image
-import time,math
+import time,math, colorsys
 
-data = np.zeros((64,64,3), dtype=np.uint8)
-
-step = 0
-
-period = 1440 / (2*math.pi)
-
-alpha = 7
-bravo = 13
-charlie = period / alpha / bravo
-
-print alpha,",",bravo,",",charlie
-
-while True:
-
-    r = ((math.sin(math.radians(step/alpha)) + 1) / 2) * 255
-    b = ((math.sin(math.radians(step/bravo)) + 1) / 2) * 255
-    g = ((math.sin(math.radians(step/charlie)) + 1) / 2) * 255
+DAY_LENTGH = 1440.0
 
 
+def compute_color(minute_of_the_day, saturation_min = 0.25, value_min_light = 0.25, value_phase=0.33 ):
 
-    print step, " : ",(r,g,b)
+    h = (2 * step / DAY_LENTGH) % 1
+    s = saturation_min + (0.5 * (1 - saturation_min) * (math.cos((step / (DAY_LENTGH / 2.)) * 2 *math.pi)+1))
+    v = value_min_light + (0.5 * (1 - value_min_light) * (math.cos( ((step + DAY_LENTGH * value_phase)/(DAY_LENTGH)) * 2 * math.pi)+1))
 
-    data[:,:,0] = int(r)
-    data[:,:,1] = int(g)
-    data[:,:,2] = int(b)
+    return [ int(i * 255) for i in colorsys.hsv_to_rgb(h,s,v) ]
 
-    img = Image.fromarray(data,'RGB')
-    img.save('render.png')
 
-    step = (step + 1) % 1440
+if __name__ == '__main__':
+    from PIL import Image
 
-    time.sleep(0.02)
+    data = np.zeros((64,64,3), dtype=np.uint8)
+    step = 0
+    while True:
+
+        r, g, b = compute_color(step)
+
+        hour = step / 60
+        minutes = step % 60
+
+        print step, " - ", hour,"h",minutes," : ",(r,g,b)
+
+        data[:,:,0] = int(r)
+        data[:,:,1] = int(g)
+        data[:,:,2] = int(b)
+
+        img = Image.fromarray(data,'RGB')
+        img.save('render.png')
+
+        step = (step + 1) % 1440
+
+        time.sleep(0.01)
